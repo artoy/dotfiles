f2e2d8db428e8383ee0800a41478d54cdf3b0418
artoy <r-b-kobayashi@mercari.com>
2024-04-01 18:27:03 +0900

    add setup

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+rm ~/.config/git
+ln -s $SCRIPT_DIR/git ~/.config/git
+
+rm ~/.config/nvim
+ln -s $SCRIPT_DIR/nvim ~/.config/nvim
+
+rm ~/.vimrc
+ln -s $SCRIPT_DIR/.vimrc ~/.vimrc
+
+rm ~/.zshrc
+ln -s $SCRIPT_DIR/.zshrc ~/.zshrc
+
+rm ~/.config/starship.toml
+ln -s $SCRIPT_DIR/starship.toml ~/.config/starship.toml
+
+source ~/.zshrc

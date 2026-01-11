57e6cb77378a2f4f710f23b6f46db0ff843bf02e
artoy <ryota5884@icloud.com>
2026-01-11 22:11:53 +0900

    fix telescope issue

--- nvim/lua/plugins/telescope.conf.lua
+++ nvim/lua/plugins/telescope.conf.lua
@@ -1,8 +1,9 @@
 local T = {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim',
     dependencies = {
         'nvim-lua/plenary.nvim',
         'delphinus/telescope-memo.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     }
 }
 

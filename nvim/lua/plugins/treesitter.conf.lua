ef7273b41c465bc0a15c645ba63d658f240ce4f1
artoy <ryota5884@icloud.com>
2026-01-12 10:13:51 +0900

    resolve treesitter issue

--- nvim/lua/plugins/treesitter.conf.lua
+++ nvim/lua/plugins/treesitter.conf.lua
@@ -1,18 +1,31 @@
 local T = {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
+    lazy = false,
+    build = ':TSUpdate'
 }
 
 function T.config()
-	require("nvim-treesitter.config").setup({
-		highlight = {
-			enable = true
-		},
-		indent = {
-			enable = true
-		}
+	require("nvim-treesitter").setup({})
+
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "*",
+		callback = function(args)
+			local bufnr = args.buf
+
+            -- Skip not empty buffers
+			if vim.bo[bufnr].buftype ~= "" then
+				return
+			end
+
+			-- Only enable treesitter when a parser exists
+			local ok = vim.treesitter.get_parser(bufnr, nil, { error = false }) ~= nil
+			if not ok then
+				return
+			end
+
+			vim.treesitter.start(bufnr)
+			vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end,
 	})
 end
 

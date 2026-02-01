be4ceaf976775ee098ce3223ec700cda36c96a1d
artoy <ryota5884@icloud.com>
2026-02-01 11:52:42 +0900

    change marp settings

--- nvim/lua/plugins/marp.conf.lua
+++ nvim/lua/plugins/marp.conf.lua
@@ -4,11 +4,15 @@ local M = {
 }
 
 function M.config()
+    local theme_basic = vim.fn.expand("~/git/marp-themes/basic.css")
     require("marp").setup {
-      -- Optional configuration
-      marp_command = "/opt/homebrew/opt/node/bin/node /opt/homebrew/bin/marp",
-      browser = nil, -- auto-detect
-      server_mode = false, -- Use watch mode (-w)
+        -- Optional configuration
+        marp_command = "/opt/homebrew/opt/node/bin/node /opt/homebrew/bin/marp",
+        browser = nil, -- auto-detect
+        server_mode = false, -- Use watch mode (-w)
+        themes = {
+            basic = theme_basic,
+        },
     }
 end
 

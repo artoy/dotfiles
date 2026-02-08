6f87938b5e0c1e52c63a688041a02c18d51fc2cb
artoy <ryota5884@icloud.com>
2026-02-08 13:51:13 +0900

    add keybinds to select an item at nvim-cmp

--- nvim/lua/plugins/nvim-cmp.conf.lua
+++ nvim/lua/plugins/nvim-cmp.conf.lua
@@ -35,6 +35,8 @@ function C.config()
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
+      ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
